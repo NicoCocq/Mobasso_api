72a45261fe1dbcd4d9ce70bb1892266b99e14c35
Unknown <guenin.vincent4@gmail.com>
2018-10-19 09:31:15 +0200

    Remote method for /containers/{container}/saveConfig

--- common/models/container.js
+++ common/models/container.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Container) {
+  Container.saveConfig = function(req, res) {
+    var fs = require('fs');
 
+    fs.writeFileSync("server/storage/" + req.params.container.toString() + "/apk_config.txt", JSON.stringify(req.body));
+  };
+
+  Container.remoteMethod('saveConfig', {
+    accepts: [
+      {arg: 'req', type: 'object', http: { source: 'req' }},
+      {arg: 'res', type: 'object', http: { source: 'res' }}
+    ],
+    returns: {arg: 'status', type: 'string'},
+    http: {path: '/:container/saveConfig', verb: 'post'}
+  });
 };
